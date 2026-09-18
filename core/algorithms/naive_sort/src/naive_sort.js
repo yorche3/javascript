14c678fb3185b931b09edabc9d3ee5cb537c044a
yorche3 <hyaoki123@gmail.com>
2026-09-18 11:37:27 -0600

    feat(algorithms): add naive sort implementation

--- core/algorithms/naive_sort/src/naive_sort.js
+++ core/algorithms/naive_sort/src/naive_sort.js
@@ -15,3 +15,54 @@ module.exports = {
     bubbleSort,
     insertionSort
 };
+
+function selectionSort(arr) {
+    if (arr == null) return null;
+    const n = arr.length;
+    if (n <= 1) return arr;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+    return arr;
+}
+
+function bubbleSort(arr) {
+    if (arr == null) return null;
+    const n = arr.length;
+    if (n <= 1) return arr;
+    for (let i = 0; i < n - 1; i++) {
+        let swapped = false;
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+    return arr;
+}
+
+function insertionSort(arr) {
+    if (arr == null) return null;
+    const n = arr.length;
+    if (n <= 1) return arr;
+    for (let i = 1; i < n; i++) {
+        const key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
